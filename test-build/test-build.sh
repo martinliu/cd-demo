f144d0613423e29d669120227e76b579b96de564
martin <67120666@qq.com>
2016-12-02 01:30:59 +0800

    key

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -8,6 +8,6 @@
 sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
 sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
 chmod 777 ./rancher-compose
-./rancher-compose --url http://52.78.73.245:8080 --access-key 0AE22ECB99C023380EEF --secret-key TF8SE9v6sFb55NViaFnGMFm6BLSzeEhiM1FiKJQd -p python-redis-demo-build${BUILD_NUMBER} up -d
+./rancher-compose --url http://52.78.73.245:8080 --access-key 811E9C1B61479FF0BBCA --secret-key GCuVUSap2poayDiMwUhDnK8PH1tCs4XYYVrkkkDo -p python-redis-demo-build${BUILD_NUMBER} up -d
 #./rancher-compose --url http://10.0.0.5:8080 --access-key CA23527D9BE1E5855619 --secret-key GF6Q1vMsimqY8MHp6t17eqoZXcbQ8VEBcjU11z7H -p python-redis-demo-build27 up --pull -d --upgrade pyapp
 # --confirm-upgrade
