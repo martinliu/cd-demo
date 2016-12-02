c309eeb68f6f2dec97f99331d31d34280ae4e545
martin <67120666@qq.com>
2016-12-02 12:41:23 +0800

    update

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,15 +2,17 @@ stage 'Build-Docker-Image'
 node("master") {
    echo 'Check out code build image'
    git 'https://github.com/martinliu/cd-demo.git'
+   dir('./src') {
     // build new images
     echo 'Build new docker image'
-    sh 'docker build -t 172.31.7.232:5000/web-demo:b${BUILD_NUMBER} .'
+    sh 'docker build -t 172.31.7.232:5000/python-redis-demo:b${BUILD_NUMBER} .'
+}
 }
 
 stage 'Push-Image'
 node("master") {
    echo 'Push new build to registory'
-    sh 'docker push 172.31.7.232:5000/web-demo:b${BUILD_NUMBER}'
+    sh 'docker push 172.31.7.232:5000/python-redis-demo:b${BUILD_NUMBER}'
 }
 
 stage 'Depooy-QA'
@@ -18,6 +20,6 @@ node("master") {
    echo 'Build a QA evn for new build'
    dir('./test-build') {
     // run build file
-   docker info
+    sh 'sh ./test-build.sh'
 }
 }
--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -9,6 +9,6 @@ sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
 sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
 chmod 777 ./rancher-compose
 export RANCHER_CLIENT_DEBUG=true
-./rancher-compose --debug --url http://52.78.73.245:8080/v1 --access-key 811E9C1B61479FF0BBCA --secret-key GCuVUSap2poayDiMwUhDnK8PH1tCs4XYYVrkkkDo -p python-redis-demo-build${BUILD_NUMBER} up -d 
+./rancher-compose --debug --url http://52.78.73.245:8080/v1/projects/1a5 --access-key 991E0C04928D36A1394A --secret-key t5XMFcGQdZgDrZPwxqpoZVRQtUxgHWpuEn4MJN66 -p python-redis-demo-build${BUILD_NUMBER} up -d
 #./rancher-compose --url http://10.0.0.5:8080 --access-key CA23527D9BE1E5855619 --secret-key GF6Q1vMsimqY8MHp6t17eqoZXcbQ8VEBcjU11z7H -p python-redis-demo-build27 up --pull -d --upgrade pyapp
 # --confirm-upgrade
